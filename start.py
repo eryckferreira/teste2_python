93bbe1b5a021d210a176ce6ccecc5a24f43e74c1
Eryck Ferreira <132472671+eryckferreira@users.noreply.github.com>
2025-01-05 18:54:22 -0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,18 @@
+# alunos_presentes = 2
+# alunos_presentes **= 6.4
+
+# total_alunos = alunos_presentes
+
+# print(round(total_alunos, 3))
+# ----------------------------------------
+# print(max(2 , 3, 9, 23, 32,155, 541))
+# print(min(2 , 3, 9, 23, 32,155, 541))
+# print(sum((2 , 3, 9, 23, 32,155, 541)))
+
+# resultado = 10 + (20 * 4)
+
+# print(resultado)
+import math
+
+print(math.ceil(3.1))
+print(math.floor(2.1))

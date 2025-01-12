79d9937e441cf2603bc07a5ec9cdc777a17279d2
Eryck Ferreira <132472671+eryckferreira@users.noreply.github.com>
2025-01-12 16:04:17 -0300

    Add files via upload

---
+++ firstmath.py
@@ -0,0 +1,7 @@
+valor_produto: float
+
+valor_produto = float(input("Digite o valor R$ "))
+
+valorTotalProduto = valor_produto * 1.20
+
+print(f"o valor total do produto com acréscimo(imposto) é: R$ {valorTotalProduto}")
